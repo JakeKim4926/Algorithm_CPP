a47588d718096acfdd4f45de37f578d8dca66a88
Jake_Kim <wer1915@gmail.com>
2025-05-07 22:10:31 +0900

    init: init empty C++ project on VC15

---
+++ Algorithm_CPP/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s)
+{
+	bool answer = true;
+
+	cout << "Hello Cpp" << endl;
+
+	return answer;
+}
+
+int main() {
+
+
+	return 0;
+}
---
+++ Algorithm_CPP/solution.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Solution {
+public:
+	// Programmers
+	bool solution_250507_01();					// 올바른 괄호
+
+};
