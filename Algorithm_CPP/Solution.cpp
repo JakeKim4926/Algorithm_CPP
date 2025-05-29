cb2e25ab91c790b4d0c21cc62d3ae4edc83b42ba
Jake_Kim <wer1915@gmail.com>
2025-05-29 21:24:15 +0900

    feat: add 게임 맵 최단거리 on Programmers

--- Algorithm_CPP/Solution.cpp
+++ Algorithm_CPP/Solution.cpp
@@ -73,6 +73,70 @@ string Solution::solution_250528_01(vector<int> numbers)
 	return answer;
 }
 
+int Solution::solution_250529_01(vector<vector<int>> maps)
+{
+	class GameMap {
+	public:
+		int row;
+		int col;
+		int count;
+
+		GameMap(int row, int col, int count) {
+			this->row = row;
+			this->col = col;
+			this->count = count;
+		}
+	};
+
+
+	vector<vector<bool>> visit(maps.size());
+	for (int i = 0; i < maps.size(); i++) {
+		vector<bool> temp;
+		for (int j = 0; j < maps[i].size(); j++) {
+			temp.push_back(false);
+		}
+		visit[i] = temp;
+	}
+
+	if (maps[0][0] == 0)
+		return -1;
+
+	queue<GameMap> bfs;
+	bfs.push(GameMap(0, 0, 1));
+
+	int dr[] = { 0,0,1,-1 };
+	int dc[] = { 1, -1, 0,0 };
+
+	while (!bfs.empty()) {
+		GameMap temp = bfs.front();
+		bfs.pop();
+
+		if (visit[temp.row][temp.col] || !maps[temp.row][temp.col])
+			continue;
+
+		if (temp.row == maps.size() - 1 && temp.col == maps[0].size() - 1)
+			return temp.count;
+
+		visit[temp.row][temp.col] = true;
+
+		for (int i = 0; i < 4; i++) {
+			int nextRow = temp.row + dr[i];
+			int nextCol = temp.col + dc[i];
+
+			if (nextRow < 0 || nextRow >= maps.size() || nextCol < 0 || nextCol >= maps[0].size())
+				continue;
+
+			if (visit[nextRow][nextCol] || !maps[nextRow][nextCol])
+				continue;
+
+			bfs.push(GameMap(nextRow, nextCol, temp.count + 1));
+		}
+
+	}
+
+	return -1;
+}
+
 int Solution::solution_250523_01()
 {
 	// Two Pointer
--- Algorithm_CPP/solution.h
+++ Algorithm_CPP/solution.h
@@ -17,6 +17,7 @@ public:
 	bool	solution_250507_01(string s);								// 올바른 괄호
 	int		solution_250507_02(vector<int> numbers, int target);		// 타겟 넘버
 	string	solution_250528_01(vector<int> numbers);					// 가장 큰 수
+	int		solution_250529_01(vector<vector<int> > maps);				// 게임 맵 최단 거리
 
 	// Baekjoon
 	int		solution_250523_01();										// No.1253 좋다
